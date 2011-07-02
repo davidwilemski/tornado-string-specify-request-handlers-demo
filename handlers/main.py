98714a9848872027ff6a6ea0aabb5708616ce91e
David Wilemski <david@davidwilemski.com>
2011-07-02 18:35:38 -0400

    Removed references of lazy-loading

--- handlers/main.py
+++ handlers/main.py
@@ -2,6 +2,6 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, lazy-loaded handlers!")
+        self.write("Hello, Strings!")
 
 
